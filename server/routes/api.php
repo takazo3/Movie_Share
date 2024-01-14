cff6271bb7db932e06f6d29809681d6498b3a2c4
takazo3 <info@re-zero-jp.com>
2024-01-14 14:54:17 +0900

    first commit

---
+++ server/routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\FavoriteController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+//Review
+Route::get('/reviews/{media_type}/{media_id}', [ReviewController::class, 'index']);
+Route::post('/reviews', [ReviewController::class, 'store']);
+Route::delete('/review/{review}', [ReviewController::class, 'destroy']);
+Route::put('/review/{review}', [ReviewController::class, 'update']);
+Route::get('/review/{review}', [ReviewController::class, 'show']);
+
+//Comment
+Route::post('/comments', [CommentController::class, 'store']);
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+Route::put('/comments/{comment}', [CommentController::class, 'update']);
+
+//Favorite
+Route::get('/favorites', [FavoriteController::class, 'index']);
+Route::post('/favorites', [FavoriteController::class, 'toggleFavorite']);
+Route::get('/favorites/status', [FavoriteController::class, 'checkFavoriteStatus']);
